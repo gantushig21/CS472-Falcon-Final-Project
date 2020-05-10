dd93a3342d4bab5a9c7636a79846761bdece47e4
Gantushig Bayasgalan <gantushig21@gmail.com>
2020-05-10 13:16:23 -0500

    First commit: Created project

---
+++ src/main/java/edu/miu/cs/cs472/falcon/controller/JobServlet.java
@@ -0,0 +1,19 @@
+package edu.miu.cs.cs472.falcon.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "JobServlet", urlPatterns = {"/index"})
+public class JobServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("C++");
+    }
+}
